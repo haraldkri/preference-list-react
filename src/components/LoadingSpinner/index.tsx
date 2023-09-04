bc3aed308f2e5b9032c4f36961ba5d02e48911b2
Harald Kriebisch <Harald.Kriebisch@gmx.de>
2023-09-04 13:19:16 +0200

    Add loading spinner

---
+++ src/components/LoadingSpinner/index.tsx
@@ -0,0 +1,9 @@
+import {Spin} from "antd";
+import Center from "../Center";
+import React from "react";
+
+const LoadingSpinner = () =>{
+    return <Center><Spin/></Center>
+};
+
+export default LoadingSpinner
