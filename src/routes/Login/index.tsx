f8501364c18228dc904dd4583fa1728a5c03b34d
Harald Kriebisch <Harald.Kriebisch@gmx.de>
2023-09-09 21:32:09 +0200

    Update Login Layout

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,14 +1,24 @@
 import React, {FC, useContext} from 'react';
-import {Button} from "antd";
+import {Button, Space} from "antd";
 import {UserContext} from "../../context/UserContext";
+import styled from "styled-components";
+
+const Wrapper = styled(Space)`
+  padding: 5% 0;
+  margin: 0 50px;
+  width: 100%;
+  display: grid;
+  place-items: center;
+  background: ${(props) => props.theme.antd.colorBgElevated};
+`;
 
 const LoginRoute: FC = () => {
     const {login, loading} = useContext(UserContext);
 
-    return <div>
-        <p>Login Route</p>
+    return <Wrapper>
+        <span>Login</span>
         <Button type={"primary"} onClick={login} disabled={loading}>Sign in with Google</Button>
-    </div>
+    </Wrapper>
 }
 
 export default LoginRoute
